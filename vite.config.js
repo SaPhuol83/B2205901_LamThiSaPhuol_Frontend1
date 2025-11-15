dececbf3e1651487f8783e231dd6a3f743f2c163
Lam Thi Sa Phuol <lamthisaphuol@gmail.com>
2025-11-15 09:58:32 +0700

    Hien thi ds contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,14 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  assetsInclude: ["**/*.woff", "**/*.woff2"],
   server: {
     port: 3001,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      },
+    },
   },
 });
